d0d6de9ba8b2a6a8dcf59502fade2a29904cd020
bearcatjamboree <crbox@crimson.ua.edu>
2022-05-12 14:53:13 -0700

    Adding scripts for downloading YouTube transcripts and converting them the SBV and SRT.

---
+++ GetYouTubeTranscript.py
@@ -0,0 +1,62 @@
+#################################################################################################
+# importing the necessary libraries
+#################################################################################################
+import argparse
+import time
+from datetime import datetime
+from datetime import timedelta
+
+#################################################################################################
+# Get arguments from command line
+#################################################################################################
+parser = argparse.ArgumentParser(
+    description='Download a transcript from a YouTube video')
+parser.add_argument('--video_id', type=str, help='The YouTube video_id value for the video being transcribed')
+parser.add_argument('--output_folder', type=str, help="the output location to write the transcript txt")
+
+args = parser.parse_args()
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+#################################################################################################
+#  Insert frames before and after match frames to add "context"
+#################################################################################################
+def getDateString(seconds):
+    td = str(timedelta(seconds=seconds))
+    s = td.split('.')
+
+    if len(s) == 1:
+        s.append('000')
+
+    new_date = "{}.{}".format(s[0], s[1][:3])
+    return new_date
+
+# Must be a single transcript.
+transcript_list = YouTubeTranscriptApi.get_transcript(args.video_id)
+
+out_txt = args.output_folder + "/" + args.video_id + ".txt"
+out_sbv = args.output_folder + "/" + args.video_id + ".sbv"
+
+# Open output file
+ft = open(out_txt, "w+")
+fs = open(out_sbv, "w+")
+
+header=True
+
+# Iterate over all available transcripts
+for transcript in transcript_list:
+
+    if header:
+        ft.write("Start\tText\n")
+        header=False
+
+    ft.write("{}\t{}\n".format(transcript['start'], transcript['text']))
+
+    start_time = getDateString(transcript['start'])
+    end_time = getDateString(transcript['duration'])
+
+    fs.write("{},{}\n".format(start_time, end_time))
+    fs.write("{}\n\n".format(transcript['text']))
+
+ft.close()
+fs.close()
---
+++ GetYouTubeTranscript.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+echo ${machine}
+output_folder="$1"
+
+##############################################################################
+# Check for file was passed.  Show open file dialog if no argument and on Mac
+###############################################################################
+if ! [ -f "output_folder" ]; then
+    if [[ "$machine" == "Mac" ]]; then
+        output_folder=$(osascript -e 'tell application (path to frontmost application as text)
+        set output_folder to choose folder
+        POSIX path of output_folder
+        end')
+    elif [[ "$machine" == "Linux" ]]; then
+        output_folder=$(dialog --title "Choose a folder" --stdout --title "Please choose an output folder" --fselect /tmp/ 14 48)
+    elif [ "$#" -ne 1 ] || ! [ -f "$output_folder" ]; then
+        echo "Usage: $0 output_folder"
+        exit 1
+    fi
+fi
+
+echo 'Enter Video ID:'
+# shellcheck disable=SC2162
+read video_id
+
+####################################
+# Remove backslashes
+####################################
+file=$(echo "$output_folder"|tr -d '\\')
+
+python3 GetYouTubeTranscript.py --video_id "$video_id" --output_folder "$output_folder"
+
+ffmpeg -fix_sub_duration -i "$output_folder/$video_id.sbv" "$output_folder/$video_id.srt"
