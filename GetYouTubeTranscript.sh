72896ca2ad6e46daf20e83fccc5821950e69c876
bearcatjamboree <crbox@crimson.ua.edu>
2022-05-16 09:15:18 -0700

    Added language list to make processing easier

--- GetYouTubeTranscript.sh
+++ GetYouTubeTranscript.sh
@@ -28,16 +28,36 @@ if ! [ -f "output_folder" ]; then
         exit 1
     fi
 fi
-# GU9_ED1bE3s
-echo 'Enter Video ID:'
-# shellcheck disable=SC2162
-read video_id
+
+##############################################################################
+# Prompt for video_id
+###############################################################################
+if [[ "$video_id" == "" ]]; then
+    if [[ "$machine" == "Mac" ]]; then
+        video_id=$(osascript -e 'set T to text returned of (display dialog "Enter video ID:" buttons {"Cancel", "OK"} default button "OK" default answer "")')
+    elif [[ "$machine" == "Linux" ]]; then
+        video_id=$(dialog --title "Enter video ID" --inputbox "video_id:" 8 60)
+    elif [[ "$machine" == "Cygwin" ]]; then
+        video_id=$(dialog --title "Enter video ID" --inputbox "video_id:" 8 60)
+    elif [ "$#" -ne 2 ] || ! [ -f "$output_folder" ]; then
+        echo "Usage: $0 output_folder video_id"
+        exit 1
+    fi
+fi
+
+if [[ "$video_id" == "" ]]; then
+    echo "Usage: $0 output_folder video_id"
+    exit 1
+fi
 
 ####################################
 # Remove backslashes
 ####################################
 file=$(echo "$output_folder"|tr -d '\\')
 
-python3 GetYouTubeTranscript.py --video_id "$video_id" --output_folder "$output_folder" --language "es"
-python3 GetYouTubeTranscript.py --video_id "$video_id" --output_folder "$output_folder" --language "hi"
-python3 GetYouTubeTranscript.py --video_id "$video_id" --output_folder "$output_folder" --language "zh-Hans"
+languages=( "en" "es" "hi" "zh" )
+
+for lang in "${languages[@]}"
+do :
+  python3 GetYouTubeTranscript.py --video_id "$video_id" --output_folder "$output_folder" --language "$lang"
+done
