6bed68b229c4be4903622394338d92141269b264
bearcatjamboree <cboxgo@yahoo.com>
2023-01-26 09:58:09 -0800

    removed unused routines

--- FixYouTubeSubtitles.py
+++ FixYouTubeSubtitles.py
@@ -2,52 +2,6 @@
 # importing the necessary libraries
 #################################################################################################
 import argparse
-import os
-from shutil import rmtree
-
-
-#################################################################################################
-#  Create folder routine
-#################################################################################################
-def createPath(s):
-    try:
-        os.mkdir(s)
-    except OSError:
-        assert False, "Creation of the directory %s failed (The TEMP directory may already exist.)"
-
-#################################################################################################
-#  Folder cleanup routine
-#################################################################################################
-def deletePath(s):  # Dangerous! Watch out!
-    try:
-        rmtree(s, ignore_errors=False)
-    except OSError:
-        print("Deletion of the directory %s failed" % s)
-        print(OSError)
-
-#################################################################################################
-#  Print progress bar
-#################################################################################################
-def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
-    # Print New Line on Complete
-    if iteration == total:
-        print()
 
 #################################################################################################
 # Get arguments from command line
