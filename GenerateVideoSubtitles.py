dfb4e79c6e6672548546f4a9f5b02bb9157ec318
bearcatjamboree <cboxgo@yahoo.com>
2023-03-03 12:30:02 -0800

    minor code updates

--- GenerateVideoSubtitles.py
+++ GenerateVideoSubtitles.py
@@ -33,6 +33,7 @@ def deletePath(s):  # Dangerous! Watch out!
 #################################################################################################
 parser = argparse.ArgumentParser(
     description='Translate audio from video file, generate subtitles, and burn them into a new video file')
+parser.add_argument('--model', type=str, default="base", help='The language model to use')
 parser.add_argument('--input_file', type=str, help='The video file you want to generate subtitles for')
 parser.add_argument('--output_file', type=str, help="the _output location to write the subtitle file")
 parser.add_argument('--language', type=str, default="English", help="the _output location to write the subtitled video")
@@ -47,21 +48,53 @@ if not args.input_file or not args.output_file:
     quit()
 
 #################################################################################################
+#  Print progress bar
+#################################################################################################
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+#################################################################################################
 #  *** Begin main part of Program ***
 #################################################################################################
 def main():
 
-    model = whisper.load_model("base")
+    model = whisper.load_model(args.model)
+    print("Loading model: {}".format(args.model))
 
     transcribe = model.transcribe(args.input_file, fp16=False, language=args.language)
     segments = transcribe['segments']
 
+    print("Creating segments for file: {}".format(args.input_file))
+
     fh = open(args.output_file, "w+")
+
+    print("Writing captions...")
 
     for segment in segments:
 
-        print("segment: {}".format(segment))
         caption_number = int(segment['id'] + 1)
+
+        #print("segment: {}".format(segment))
+        printProgressBar(caption_number, len(segments), prefix='Inserting Margin Frames: ', suffix='Complete', length=50)
+
         startDelta = timedelta(seconds=int(segment['start']))
         endDelta = timedelta(seconds=int(segment['end']))
 
